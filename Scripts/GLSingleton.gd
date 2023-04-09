221129acd32918b7ff94b45c23c3406a0d7caa2a
BenAltermatt <ben.altermatt20@gmail.com>
2023-04-09 16:51:28 -0400

    Fixed building destroying resources. Resize sprites and background.

--- Scripts/GLSingleton.gd
+++ Scripts/GLSingleton.gd
@@ -8,24 +8,24 @@ var ops
 var won 
 
 func _ready():
-    won = false
-    ops = read_in_opps()
-    cur_op_ind = 0
+	won = false
+	ops = read_in_opps()
+	cur_op_ind = 0
 
 
 # reads in opponent informtation from a file
 func read_in_opps():
-    var ret_ops = []
+	var ret_ops = []
 
-    var file = File.new()
-    file.open("res://Assets/Files/Opponents.txt", File.READ)
-    var blocks = file.get_as_text().split(";")
-    for block in blocks:
-        if(len(block.strip_edges()) > 0):
-            ret_ops.append(Opponent.new(block.strip_edges())) 
+	var file = File.new()
+	file.open("res://Assets/Files/Opponents.txt", File.READ)
+	var blocks = file.get_as_text().split(";")
+	for block in blocks:
+		if(len(block.strip_edges()) > 0):
+			ret_ops.append(Opponent.new(block.strip_edges())) 
 
-    return ret_ops
+	return ret_ops
 
 func next_enemy(win):
-    won = win
-    get_tree().change_scene("res://Scenes/WinLose.tscn")
+	won = win
+	get_tree().change_scene("res://Scenes/WinLose.tscn")
